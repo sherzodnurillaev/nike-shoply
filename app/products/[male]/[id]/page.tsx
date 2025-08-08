f42e5624f691576b3a2d48c7d6fd2c886b4d0925
Amal <sherzod1700@mail.ru>
2025-08-08 22:38:55 +0500

    fixed bug

--- app/products/[male]/[id]/page.tsx
+++ app/products/[male]/[id]/page.tsx
@@ -1,13 +1,6 @@
 import ProductId from "@/components/Product/ProductId";
 
-interface PageProps {
-  params: {
-    male: string;
-    id: string;
-  };
-}
-
-export default async function ProductPage({ params }: PageProps) {
+export default function ProductPage({ params }: any) {
   return (
     <div className="max-w-[1300px] !mx-auto !px-[20px] !mt-[20px] md:!mt-[50px]">
       <ProductId id={params.id} />
